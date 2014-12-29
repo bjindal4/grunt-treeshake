50e66ab59063406962e24d1843fb634419d8f004
Rob Taylor <roboncode@gmail.com>
2014-12-29 11:59:24 -0700

    updated grunt example with minify option

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function (grunt) {
             demo: {
                 options: {
                     wrap: 'demo',
+                    minify: true,
                     inspect: ['example/app.js']
                 },
                 files: {
